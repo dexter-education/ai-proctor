266827c9a8ae46c0c8441a01e3107295dcf76368
vardanagarwal <vardanagarwal16@gmail.com>
2022-05-08 16:30:58 +0530

    Changing structure

--- detector.py
+++ detector.py
@@ -1,19 +1,5 @@
-# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
-"""
-Run inference on images, videos, directories, streams, etc.
-
-Usage:
-    $ python path/to/detect.py --weights yolov5s.pt --source 0  # webcam
-                                                             img.jpg  # image
-                                                             vid.mp4  # video
-                                                             path/  # directory
-                                                             path/*.jpg  # glob
-                                                             'https://youtu.be/Zgi9g1ksQHc'  # YouTube
-                                                             'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP stream
-"""
-
-from pathlib import Path
-
+import cv2
+from mtcnn.mtcnn import MTCNN
 import numpy as np
 import torch
 
@@ -24,10 +10,11 @@ from utils.torch_utils import select_device
 
 torch.cuda.empty_cache()
 
-class infer_single:
+class yolov5_infer_single:
 
     def __init__(self,
                  weights,
+                 confidence=0.25,
                  imgsz=640,  # inference size (pixels)
                  half=False  # use FP16 half-precision inference
                  ):
@@ -37,12 +24,13 @@ class infer_single:
         self.model = DetectMultiBackend(weights, device=self.device, dnn=False, fp16=half)
         self.stride, self.names, self.pt = self.model.stride, self.model.names, self.model.pt
         self.imgsz = check_img_size(imgsz, s=self.stride)  # check image size
+        self.confidence = confidence
 
         # Run inference
         self.model.warmup(imgsz=(1, 3, self.imgsz, self.imgsz))  # warmup
 
     @torch.no_grad()
-    def detect(self, img, conf=0.25):
+    def detect(self, img):
 
         img = letterbox(img, self.imgsz, stride=self.stride, auto=self.pt)[0]
         img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
@@ -56,8 +44,8 @@ class infer_single:
         # Inference
         pred = self.model(im, augment=False, visualize=False)
         # NMS
-        pred = non_max_suppression(pred, conf, 0.45, None, False, max_det=100)
-        labels_dict = {0: 0, 76: 0, 72: 0} # person, movile phone and laptop
+        pred = non_max_suppression(pred, self.confidence, 0.45, None, False, max_det=100)
+        labels_dict = {0: 0, 67: 0, 63: 0} # person, mobile phone and laptop
         for det in pred:  # per image
             if len(det):
                 # Print results
@@ -66,4 +54,19 @@ class infer_single:
                         n = (det[:, -1] == c).sum()  # detections per class
                         labels_dict[c] = n
         
-        return list(labels_dict.values())
+        return dict(zip(['person', 'mobile phone', 'laptop'], list(labels_dict.values()))) # list returning counts of person, mobile phone and laptop
+
+class mtcnn_face:
+    """Class to count faces in the image
+    """
+    def __init__(self, confidence=0.95):
+        self.model = MTCNN()
+        self.confidence = confidence
+
+    def detect(self, img):
+
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        res = self.model.detect_faces(img_rgb)
+        face_count = sum([1 if face['confidence'] > self.confidence else 0 for face in res])
+
+        return {'face': face_count} # dictionary returning count of faces in image
--- main_runner.py
+++ main_runner.py
@@ -1,59 +1,24 @@
 import cv2
-from mtcnn.mtcnn import MTCNN
-from yolov5_single_detector import infer_single
 
-class count:
-    """Class to count and raise flags
-    """
-
-    def __init__(self):
-
-        self.MIN_CONTINOUS_FRAMES = [10, 10, 10, 10]
-        self.frame_counter = [0, 0, 0, 0]
-        self.required_counts = [1, 1, 0, 0]
-        self.level = ['red', 'red', 'red', 'red']
-        self.message = ['face count violation', 'person count violation', 'mobile phone violation', 'laptop violation']
-
-    def flag(self, level, frame_num, message):
-        print(level, frame_num, message)
-
-    def check_counts(self, counts, frame_num):
-
-        for i in range(len(counts)):
-            
-            if self.frame_counter[i] > self.MIN_CONTINOUS_FRAMES[i]:
-                self.flag(self.level[i], frame_num - self.MIN_CONTINOUS_FRAMES[i], self.message[i])
-                self.frame_counter[i] = 0
-
-            if counts[i] != self.required_counts[i]:
-                self.frame_counter[i] += 1
-            else:
-                self.frame_counter[i] = 0
-
-def face_count(img):
-    """Function to count faces in the image
-    """
-    model = MTCNN()
-
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    res = model.detect_faces(img_rgb)
-    face_count = sum([1 if face['confidence'] > 0.95 else 0 for face in res])
-
-    return face_count
+import config
+import detector
+import flagger
 
 if __name__ == "__main__":
 
     cap = cv2.VideoCapture(0)
     frame_num = 0
-    yolov5_obj = infer_single('yolov5s.pt', half=False)
-    count_obj = count()
+    config_dict = config.get_config()
+    yolov5_obj = detector.yolov5_infer_single('yolov5s.pt', half=False)
+    mtcnn_obj = detector.mtcnn_face()
+    count_obj = flagger.flagger(config_dict)
     
     while True:
         ret, img = cap.read()
         if ret:
-            counts = [face_count(img)]
-            counts.extend(yolov5_obj.detect(img))
-            count_obj.check_counts(counts, frame_num)
+            count_obj.add_count(mtcnn_obj.detect(img))
+            count_obj.add_count(yolov5_obj.detect(img))
+            count_obj.check_counts(frame_num)
             cv2.imshow('img', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
