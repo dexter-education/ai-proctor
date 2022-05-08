f0fa1cc76afab9836bbc09647ca440864d8d9ffd
vardanagarwal <vardanagarwal16@gmail.com>
2022-05-08 16:48:53 +0530

    Adding class

--- main_runner.py
+++ main_runner.py
@@ -4,26 +4,37 @@ import config
 import detector
 import flagger
 
+class runner:
+
+    def __init__(self, yolov5_model='yolov5s.pt', frame_num=0):
+        
+        self.frame_num = frame_num
+        self.config_dict = config.get_config()
+        self.yolov5_obj = detector.yolov5_infer_single(yolov5_model)
+        self.mtcnn_obj = detector.mtcnn_face()
+        self.count_obj = flagger.flagger(self.config_dict)
+
+    def run(self, img):
+
+        self.count_obj.add_count(self.mtcnn_obj.detect(img))
+        self.count_obj.add_count(self.yolov5_obj.detect(img))
+        self.count_obj.check_counts(self.frame_num)
+        self.frame_num += 1
+
+
 if __name__ == "__main__":
 
     cap = cv2.VideoCapture(0)
-    frame_num = 0
-    config_dict = config.get_config()
-    yolov5_obj = detector.yolov5_infer_single('yolov5s.pt', half=False)
-    mtcnn_obj = detector.mtcnn_face()
-    count_obj = flagger.flagger(config_dict)
+    runner_obj = runner()
     
     while True:
         ret, img = cap.read()
         if ret:
-            count_obj.add_count(mtcnn_obj.detect(img))
-            count_obj.add_count(yolov5_obj.detect(img))
-            count_obj.check_counts(frame_num)
+            runner_obj.run(img)
             cv2.imshow('img', img)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
             break
-        frame_num += 1
     cap.release()
     cv2.destroyAllWindows()
