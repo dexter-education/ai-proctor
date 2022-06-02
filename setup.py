8b24f2a05c9dfc791359af44541e95c16fe4d699
vardanagarwal <vardanagarwal16@gmail.com>
2022-06-02 18:29:36 +0530

    Bumping version to 0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dexter-ai-proctor",
-    version="0.2",
+    version="0.3",
     author="Vardan Agarwal",
     author_email="vardanagarwal16@gmail.com",
     description="A package for AI proctoring of dexter learning.",
